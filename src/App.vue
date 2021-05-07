3334a97c0cdb325fa66ad0358d365c2b172c19d8
Harry Seigne <hgs30@uclive.ac.nz>
2021-05-08 00:37:27 +1200

    updated filename

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Clipboard logo" src="./assets/logo.png">
+    <clipboard/>
   </div>
 </template>
 
 <script>
-import clipboard from './components/HelloWorld.vue'
+import clipboard from './components/clipboard.vue'
 
 export default {
   name: 'App',
